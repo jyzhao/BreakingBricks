b13d8d78c7d7d8fb30b1ada7f452f9e80dff8836
Jiyuan Zhao <zhao89@purdue.edu>
2014-04-23 12:42:45 -0400

    Initial Commit

--- BreakingBricks/MyScene.m
+++ BreakingBricks/MyScene.m
@@ -8,43 +8,78 @@
 
 #import "MyScene.h"
 
+@interface MyScene ()
+
+@property (nonatomic) SKSpriteNode *paddle;
+
+@end
+
 @implementation MyScene
+
+- (void)addBall:(CGSize)size {
+	SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+	[self addChild:ball];
+	
+	CGPoint ballPoint = CGPointMake(size.width/2, size.height/2);
+	ball.position = ballPoint;
+	
+	ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+	
+	ball.physicsBody.linearDamping = 0;
+	ball.physicsBody.friction = 0;
+	ball.physicsBody.restitution = 1;
+	
+	
+	CGVector myVector = CGVectorMake(5, 5);
+	
+	[ball.physicsBody applyImpulse:myVector];
+}
+
+-(void)addPlayer:(CGSize)size {
+	
+	self.paddle = [SKSpriteNode spriteNodeWithImageNamed:@"paddle"];
+	self.paddle.position = CGPointMake(size.width/2, self.paddle.frame.size.height/2);
+	self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
+	self.paddle.physicsBody.dynamic = NO;
+	
+	[self addChild:self.paddle];
+	
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+	
+	for (UITouch *touch in touches) {
+		CGPoint location = [touch locationInNode:self];
+		CGPoint newPosition = CGPointMake(location.x, self.paddle.frame.size.height/2);
+		
+		newPosition.x = (newPosition.x < self.paddle.frame.size.width/2) ? self.paddle.frame.size.width/2 : newPosition.x;
+		newPosition.x = (newPosition.x > 320 - self.paddle.frame.size.width/2) ? 320 - self.paddle.frame.size.width/2 : newPosition.x;
+		
+		self.paddle.position = newPosition;
+		
+	}
+	
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+		self.backgroundColor = [SKColor whiteColor];
+		
+		self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+		self.physicsWorld.gravity = CGVectorMake(0, 0);
+		
+		
+		[self addBall:size];
+		[self addPlayer:size];
+		
+		
+		
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
-}
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
